ac68cf4da2976ba542cec3f2e39023d93d7313b3
RootDevelop <land@flightdeck.to>
2021-08-14 11:08:24 +0200

    Added mobiflight 2 enum converter

---
+++ Mobiflight2Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace Mobiflight2Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("events.txt");
+
+            var sw = new StringWriter();
+
+            foreach (var line in lines)
+            {
+                // Skip comment ines
+                if (line.Contains("//")) continue;
+
+                // Skip empty lines
+                if (string.IsNullOrWhiteSpace(line.Trim())) continue;
+
+                var command = line.Split("#")[0];
+
+                sw.WriteLine($"MOBIFLIGHT_{command.Replace(".", "__")},");
+            }
+
+            File.WriteAllText("enums.txt", sw.ToString());
+        }
+    }
+}
